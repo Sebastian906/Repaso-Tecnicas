07193fc9782a8243391ecb7dadae8e111dadc081
Sebastian906 <elsebas1912@gmail.com>
2025-09-24 19:55:00 -0500

    Se renombran archivos de interfaz para Java

--- Java/Ejercicio 2/InterfazConsola2.java
+++ Java/Ejercicio 2/InterfazConsola2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InterfazConsola {
+public class InterfazConsola2 {
     public static void main(String[] args) {
         GestionPlatos gestionPlatos = new GestionPlatos();
         GestionPedidos gestionPedidos = new GestionPedidos();
